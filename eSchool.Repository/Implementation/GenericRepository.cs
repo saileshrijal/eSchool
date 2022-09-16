9f5a11e3ace971d2716bbec2f7d9db7f267e1a9b
saileshrijal <saileshrijal1@gmail.com>
2022-09-16 22:15:13 +0545

    generic repository interfaces added

--- eSchool.Repository/Implementation/GenericRepository.cs
+++ eSchool.Repository/Implementation/GenericRepository.cs
@@ -1,1 +1,57 @@
-﻿
+﻿using System.Linq.Expressions;
+using eSchool.Repository.Interface;
+
+namespace eSchool.Repository.Implementation;
+
+public class GenericRepository<T> : IDisposable, IGenericRepository<T> where T:class
+{
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "")
+    {
+        throw new NotImplementedException();
+    }
+
+    public T GetById(object id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task GetByIdAsync(object id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> DeleteAsync(object id)
+    {
+        throw new NotImplementedException();
+    }
+}
--- eSchool.Repository/Interface/IGenericRepository.cs
+++ eSchool.Repository/Interface/IGenericRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace eSchool.Repository.Interface;
+﻿using System.Linq.Expressions;
 
-public interface IGenericRepository
+namespace eSchool.Repository.Interface;
+
+public interface IGenericRepository<T> : IDisposable where T:class
 {
-    
+    IEnumerable<T> GetAll(
+        Expression<Func<T, bool>> filter = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        string includeProperties = "");
+
+    T GetById(Object id);
+    Task GetByIdAsync(Object id);
+    void Add(T entity);
+    Task<T> AddAsync(T entity);
+    void Update(T entity);
+    Task<T> UpdateAsync(T entity);
+    void Delete(T entity);
+    Task<T> DeleteAsync(Object id);
 }
