8d7e4ce0fb196dc53b34a2a69b0a0d140306bb44
saileshrijal <saileshrijal1@gmail.com>
2022-09-17 14:46:55 +0545

    grade view model added

---
+++ eSchool.ViewModels/GradeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using eSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSchool.ViewModels
+{
+    public class GradeViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public GradeViewModel()
+        {
+
+        }
+
+        public GradeViewModel(Grade model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+            Description = model.Description;
+        }
+
+        public Grade ConvertViewModel(GradeViewModel model)
+        {
+            return new Grade
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Description = model.Description
+            };
+        }
+    }
+}
