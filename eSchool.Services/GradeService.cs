7e84fcaf9d1712d402a9d4eb3f54539a9bf663cd
saileshrijal <saileshrijal1@gmail.com>
2022-09-22 17:34:58 +0545

    garde service completed

---
+++ eSchool.Services/GradeService.cs
@@ -0,0 +1,84 @@
+﻿using eSchool.Models;
+using eSchool.Repository.Interface;
+using eSchool.Utilities;
+using eSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSchool.Services
+{
+    public class GradeService : IGradeService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GradeService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void DeleteGrade(int id)
+        {
+            var model = _unitOfWork.GenericRepository<Grade>().GetById(id);
+            _unitOfWork.GenericRepository<Grade>().Delete(model);
+            _unitOfWork.save(); 
+        }
+
+        public PagedResult<GradeViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            var vm = new GradeViewModel();
+            int totalCount;
+            List<GradeViewModel> vmList = new List<GradeViewModel>();
+            try
+            {
+                int excludeRecords = (pageSize * pageNumber) - pageSize;
+                var modelList = _unitOfWork.GenericRepository<Grade>().GetAll()
+                                    .Skip(excludeRecords).Take(pageSize).ToList();
+                totalCount = _unitOfWork.GenericRepository<Grade>().GetAll().ToList().Count;
+                vmList = ConvertModelToViewModelList(modelList);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            var result = new PagedResult<GradeViewModel>
+            {
+                Data = vmList,
+                TotalItems = totalCount,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+            return result;  
+        }
+
+        private List<GradeViewModel> ConvertModelToViewModelList(List<Grade> modelList)
+        {
+            return modelList.Select(x => new GradeViewModel(x)).ToList();
+        }
+
+        public GradeViewModel GetGradeById(int id)
+        {
+            var model = _unitOfWork.GenericRepository<Grade>().GetById(id);
+            var vm = new GradeViewModel(model);
+            return vm;
+        }
+
+        public void InsertGrade(GradeViewModel model)
+        {
+            var grade = new GradeViewModel().ConvertViewModel(model);
+            var vm = new GradeViewModel(grade);
+            _unitOfWork.save();
+        }
+
+        public void UpdateGrade(GradeViewModel model)
+        {
+            var grade = new GradeViewModel().ConvertViewModel(model);
+            var modelById = _unitOfWork.GenericRepository<Grade>().GetById(grade.Id);
+            modelById.Name = model.Name;
+            _unitOfWork.save();
+        }
+    }
+}
---
+++ eSchool.Services/IGradeService.cs
@@ -0,0 +1,19 @@
+﻿using eSchool.Utilities;
+using eSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSchool.Services
+{
+    public interface IGradeService
+    {
+        PagedResult<GradeViewModel> GetAll(int pageNumber, int pageSize);
+        GradeViewModel GetGradeById(int id);
+        void UpdateGrade(GradeViewModel model);
+        void DeleteGrade(int id);
+        void InsertGrade(GradeViewModel model); 
+    }
+}
